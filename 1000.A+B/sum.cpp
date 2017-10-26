a7ae91cc4abc1e47d9bb5d63f09ebfc482e9ff24
Taemin Hong <taeminhong@outlook.com>
2017-10-26 14:07:17 +0900

    1000번 문제 해결

---
+++ 1000.A+B/sum.cpp
@@ -0,0 +1,24 @@
+﻿/*
+URL: https://www.acmicpc.net/problem/1000
+문제: 두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+입력: 첫째 줄에 A와 B가 주어진다. (0 < A,B < 10)
+출력: 첫째 줄에 A+B를 출력한다.
+예제 입력: 1 2
+예제 출력: 3
+결과: 통과
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << sum(a, b);
+}
