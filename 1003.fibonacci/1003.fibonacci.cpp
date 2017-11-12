3c7f4bdecc6a43a16dc7cb608e2cb9ffb4f7a1b1
Taemin Hong <taeminhong@outlook.com>
2017-11-12 13:06:42 +0900

    1003: 문제 설명 축약

--- 1003.fibonacci/1003.fibonacci.cpp
+++ 1003.fibonacci/1003.fibonacci.cpp
@@ -1,38 +1,17 @@
 /*
 URL: https://www.acmicpc.net/problem/1003
-문제:
-	  fibonacci(3)을 호출하면 다음과 같은 일이 일어난다.
-	  fibonacci(3)은 fibonacci(2)와 fibonacci(1) (첫 번째 호출)을 호출한다.
-	  fibonacci(2)는 fibonacci(1) (두 번째 호출)과 fibonacci(0)을 호출한다.
-	  두 번째 호출한 fibonacci(1)은 1을 출력하고 1을 리턴한다.
-	  fibonacci(0)은 0을 출력하고, 0을 리턴한다.
-	  fibonacci(2)는 fibonacci(1)과 fibonacci(0)의 결과를 얻고, 1을 리턴한다.
-	  첫 번째 호출한 fibonacci(1)은 1을 출력하고, 1을 리턴한다.
-	  fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한다.
-	  이 때, 1은 2번 출력되고, 0은 1번 출력된다. N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
-제공함수:
-	int fibonacci(int n) {
-		if (n==0) {
-			printf("0");
-			return 0;
-		} else if (n==1) {
-			printf("1");
-			return 1;
-		} else {
-			return fibonacci(n‐1) + fibonacci(n‐2);
-		}
+문제: 40과 같거나 작은N이 주어졌을 때, fibonacci(N)이 0과 1을 각각 몇 번 출력되는지 구하시오.
+int fibonacci(int n) {
+	if (n==0) {
+		printf("0");
+		return 0;
+	} else if (n==1) {
+		printf("1");
+		return 1;
+	} else {
+		return fibonacci(n‐1) + fibonacci(n‐2);
 	}
-입력: 첫째 줄에 N이 주어진다. (N <= 40, N == 0)
-출력: 각 테스트 케이스마다 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력한다.
-예제 입력:
-  3
-  0
-  1
-  3
-예제 출력:
-  1 0
-  0 1
-  1 2
+}
 결과: 성공
 */
 #include <iostream>
